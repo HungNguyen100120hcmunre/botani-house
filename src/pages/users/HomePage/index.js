ee1ab2d072c1551f38ecd9f4f960537ef427c71b
John Smith <0950080122@sv.hcmunre.edu.vn>
2025-07-01 00:46:13 +0700

    1/7

--- src/pages/users/HomePage/index.js
+++ src/pages/users/HomePage/index.js
@@ -1,11 +1,7 @@
 
 const Home = () => {
   return (
-    <div style={{ padding: '20px' }}>
-      <h2>alo</h2>
-      <h2>Body</h2>
-      <h2>hihi</h2>
-    </div>
+    <h2>Body</h2>
   );
 };
 
--- src/router.js
+++ src/router.js
@@ -1,29 +1,34 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/users/HomePage'; // đường dẫn đúng đến Home
+import Profile from './pages/users/ProfilePage';
 import MasterLayout from './pages/users/theme/masterLayout';
 import { ROUTERS } from './utils/router';
 
 const renderUserRouter = () => {
-    const userRouters = [
-        {
-            path: ROUTERS.USER.HOME,
-            component: <Home/>
-        },
-    ]
+  const userRouters = [
+    {
+      path: ROUTERS.USER.HOME,
+      component: <Home />
+    },
+    {
+      path: ROUTERS.USER.PROFILE,
+      component: <Profile />
+    },
+  ]
 
-    return (
-        <MasterLayout>
-    <Routes>
-      {userRouters.map((item, key) => (
-        <Route key={key} path={item.path} element={item.component} />
-      ))}
-    </Routes>
+  return (
+    <MasterLayout>
+      <Routes>
+        {userRouters.map((item, key) => (
+          <Route key={key} path={item.path} element={item.component} />
+        ))}
+      </Routes>
     </MasterLayout>
   );
 };
 
 const RouterCustom = () => {
-    return renderUserRouter();
+  return renderUserRouter();
 }
 
 export default RouterCustom;
